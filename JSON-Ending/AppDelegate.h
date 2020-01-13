a4c8768ce33cab8fb01ddfec1f20fc0d4c39500e
AnatolyRyavkin <toryavkin@gmail.com>
2020-01-13 23:36:24 +0300

    Initial Commit

---
+++ JSON-Ending/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSON-Ending
+//
+//  Created by Anatoly Ryavkin on 13.01.2020.
+//  Copyright © 2020 AnatolyRyavkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSON-Ending/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JSON-Ending
+//
+//  Created by Anatoly Ryavkin on 13.01.2020.
+//  Copyright © 2020 AnatolyRyavkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
